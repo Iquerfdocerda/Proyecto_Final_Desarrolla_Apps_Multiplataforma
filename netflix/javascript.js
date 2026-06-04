56f23a13d70091e11233851e309e04a8e8c6f483
Iquerfdocerda <iquerfdocerda@gmail.com>
2026-06-04 08:24:06 -0600

    Delete netflix directory
    Version incorrecta

--- netflix/javascript.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function(e) {
-    const user = document.getElementById('username').value;
-    console.log("Intentando iniciar sesión con el usuario: " + user);
-});
--- netflix/register_script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('registerForm').addEventListener('submit', function(e) {
-    const pass = document.getElementById('pass').value;
-    const confPass = document.getElementById('conf_pass').value;
-    if (pass !== confPass) {
-        e.preventDefault(); 
-        alert("¡Las contraseñas no coinciden! Revisa de nuevo.");
-    }
-});
